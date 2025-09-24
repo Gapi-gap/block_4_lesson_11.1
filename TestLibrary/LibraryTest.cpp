b34598f7b465fd36a1a56a2b60f8397d097c02a5
artem <artem.gaponov.2024@mail.ru>
2025-09-25 02:30:49 +0300

    Исправление

---
+++ TestLibrary/LibraryTest.cpp
@@ -0,0 +1,12 @@
+﻿#include"framework.h"
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	std::cout << "Введите имя: ";
+	std::string name;
+	std::cin >> name;
+	Greeter My;
+	std::cout << My.greet(name) << std::endl;
+	return EXIT_SUCCESS;
+}
